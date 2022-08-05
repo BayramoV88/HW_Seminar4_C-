7e3a0c31bb959a9a6be6febe7f208b2c8081f795
Vadim Bayramov <bayramovva@gmail.com>
2022-08-06 00:56:34 +0300

    Add Zadacha27

---
+++ Zadacha27/Program.cs
@@ -0,0 +1,20 @@
+﻿void Zadacha27()
+{
+    //Напишите программу, которая принимает на вход число
+    //и выдаёт сумму цифр в числе
+
+    Console.WriteLine("Введите число");
+    int num = Convert.ToInt32(Console.ReadLine());
+    int sum = 0;
+    int i = 0;
+
+    while (num > 0)
+    {
+        i = num % 10;
+        sum = sum + i;
+        num = num / 10;
+    }
+    Console.WriteLine($"Сумма цифр введенного числа равна {sum}");
+}
+
+Zadacha27();
