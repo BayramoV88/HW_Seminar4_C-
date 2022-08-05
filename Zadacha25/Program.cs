74d37918e16275ba4bba25931a3c9de6b7fb8dba
Vadim Bayramov <bayramovva@gmail.com>
2022-08-05 23:58:24 +0300

    Add Zadacha25

---
+++ Zadacha25/Program.cs
@@ -0,0 +1,27 @@
+﻿void Zadacha25()
+{
+    Console.WriteLine("Введите первое число");
+    int numA = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число");
+    int numB = Convert.ToInt32(Console.ReadLine());
+    int prod = numA * numA;
+
+    if (numB == 1)
+    {
+        Console.WriteLine($"число {numA} в степени {numB} равно {numA}");
+    }
+    else if (numB == 2)
+    {
+        Console.WriteLine($"число {numA} в степени {numB} равно {numA * numA}");
+    }
+    else
+    {
+        for (int i = 3; i <= numB; i++)
+        {
+            prod = prod * numA;
+        }
+        Console.WriteLine($"число {numA} в степени {numB} равно {prod}");
+    }
+}
+
+Zadacha25();
